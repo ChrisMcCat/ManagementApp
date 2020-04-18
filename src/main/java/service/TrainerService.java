65c546df86ce5715f37b1649b5ab37393e3aa127
ChrisMcCat <ichris.kk@gmail.com>
2020-04-18 14:46:24 +0200

    Added TrainerService findById method

--- src/main/java/service/TrainerService.java
+++ src/main/java/service/TrainerService.java
@@ -1,4 +1,25 @@
 package service;
 
+import dao.TrainerDao;
+import domain.Trainer;
+import dto.TrainerDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+
 public class TrainerService {
+
+	private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
+
+	@Inject
+	private TrainerDao trainerDao;
+
+	@Inject
+	private TrainerMapper trainerMapper;
+
+	public TrainerDto findById(long id) {
+		Trainer trainer = trainerDao.findById(id).orElseThrow();
+		return trainerMapper.mapEntityToDto(trainer);
+	}
 }
